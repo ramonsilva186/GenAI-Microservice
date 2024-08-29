13d1c5759bdc868cf71a0b5e14f7648edb90eba9
ramonsilva186 <ramonsilva186@gmail.com>
2024-08-28 21:56:25 -0300

    create services

--- src/main/java/infrastructure/repositories/HibernateCustomerRepository.java
+++ src/main/java/infrastructure/repositories/HibernateCustomerRepository.java
@@ -4,9 +4,11 @@ import domain.models.Customer;
 import domain.models.ProfilePhoto;
 import domain.repositories.CustomerQuery;
 import domain.repositories.CustomerRepository;
+import jakarta.enterprise.context.ApplicationScoped;
 
 import java.util.List;
 
+@ApplicationScoped
 public class HibernateCustomerRepository implements CustomerRepository {
     @Override
     public List<Customer> find(CustomerQuery query) {
